499e380a9aa1a8e75cea6b2168afda09002e5b7f
Raghu Betina <raghu@firstdraft.com>
2016-12-01 16:38:32 +0000

    Direct association between cities and restaurants

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,9 @@
 class City < ApplicationRecord
   # Direct associations
 
+  has_many   :restaurants,
+             :dependent => :destroy
+
   has_many   :routes,
              :foreign_key => "cities_id",
              :dependent => :destroy
