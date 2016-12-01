644681b695dd3d6cb8c3e0cfcc1b5197f46b2440
Raghu Betina <raghu@firstdraft.com>
2016-12-01 16:39:02 +0000

    Whitelist columns in Preference for ActiveAdmin

--- app/admin/preference.rb
+++ app/admin/preference.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Preference do
 
+ permit_params :taste, :style
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
