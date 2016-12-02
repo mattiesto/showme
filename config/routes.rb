0f449aee9863fa5c2d2060ca5f1bf49ba1931c81
Raghu Betina <raghu@firstdraft.com>
2016-12-02 15:35:25 +0000

    3rd draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root :to => "users#index"
+  root :to => "restaurants#index"
   # Routes for the Preference resource:
   # CREATE
   get "/preferences/new", :controller => "preferences", :action => "new"
@@ -86,22 +87,12 @@ Rails.application.routes.draw do
   get "/delete_restaurant/:id", :controller => "restaurants", :action => "destroy"
   #------------------------------
 
+  devise_for :users
   # Routes for the User resource:
-  # CREATE
-  get "/users/new", :controller => "users", :action => "new"
-  post "/create_user", :controller => "users", :action => "create"
-
   # READ
   get "/users", :controller => "users", :action => "index"
   get "/users/:id", :controller => "users", :action => "show"
 
-  # UPDATE
-  get "/users/:id/edit", :controller => "users", :action => "edit"
-  post "/update_user/:id", :controller => "users", :action => "update"
-
-  # DELETE
-  get "/delete_user/:id", :controller => "users", :action => "destroy"
-  #------------------------------
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
